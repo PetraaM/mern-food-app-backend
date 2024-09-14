9f6cb5bdfadb80ac31c916f91c6d8928cc91ff81
Petra <milicevicpetra7@gmail.com>
2024-09-14 11:20:01 +0200

    validation

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 
@@ -14,6 +15,8 @@ const upload = multer({
 
 router.post(
   "/",
+  jwtCheck,
+  jwtParse,
   upload.single("imageFile"),
   MyRestaurantController.createMyRestaurant
 );
